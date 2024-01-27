f1bf5c0e88177e0b379cf9c43e3bf94da9b88115
savchenkoadd <savchenkooad@gmail.com>
2024-01-27 21:50:20 +0200

    Add Identity database context

---
+++ src/Flashcards.WebAPI/Flashcards.Infrastructure/Db/AccountDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Flashcards.Core.Domain.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flashcards.Infrastructure.Db
+{
+	public class AccountDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+	{
+        public AccountDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public AccountDbContext()
+        {
+        }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+		}
+	}
+}
