e96d3d7768969b14cb8c76b688e2e2091e1530f4
savchenkoadd <savchenkooad@gmail.com>
2024-01-27 19:55:24 +0200

    Add Flashcard entity

---
+++ src/Flashcards.WebAPI/Flashcards.Core/Entities/Flashcard.cs
@@ -0,0 +1,19 @@
+﻿namespace Flashcards.Core.Entities
+{
+	public class Flashcard
+	{
+		public Guid CardId { get; set; }
+
+		public Guid UserId { get; set; }
+
+		public float EFactor { get; set; } = 2.5f;
+
+		public int RepetitionCount { get; set; } = 0;
+
+		public int RepeatInDays { get; set; } = 0;
+
+		public string MainSide { get; set; }
+
+		public string OppositeSide { get; set; }
+	}
+}
