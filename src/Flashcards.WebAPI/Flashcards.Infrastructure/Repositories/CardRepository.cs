3a2f9af8085a4989b627b358db6c1c5fc08af455
savchenkoadd <savchenkooad@gmail.com>
2024-01-27 20:08:02 +0200

    Add CardRepository

---
+++ src/Flashcards.WebAPI/Flashcards.Infrastructure/Repositories/CardRepository.cs
@@ -0,0 +1,46 @@
+﻿using Flashcards.Core.Domain.Entities;
+using Flashcards.Core.Domain.RepositoryContracts;
+using MongoDB.Driver;
+using System.Linq.Expressions;
+
+namespace Flashcards.Infrastructure.Repositories
+{
+	public class CardRepository : IRepository<Flashcard>, ICardRepository
+	{
+		private readonly IMongoCollection<Flashcard> _flashcardsCollection;
+		private readonly FilterDefinitionBuilder<Flashcard> _builder;
+
+        public CardRepository(
+				IMongoDatabase mongoDatabase, string collectionName
+			)
+        {
+			_flashcardsCollection = mongoDatabase.GetCollection<Flashcard>(collectionName);
+			_builder = Builders<Flashcard>.Filter;
+        }
+
+        public Task CreateAsync(Flashcard entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<bool> DeleteAsync(Guid id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IReadOnlyCollection<Flashcard>> GetAllAsync(Expression<Func<Flashcard, bool>> expression)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<int> SyncCards(List<Flashcard> flashcards)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<int> UpdateAsync(Guid id, Flashcard entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
