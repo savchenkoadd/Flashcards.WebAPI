08ccf22a81dda1c81036c2a4a6ff1237500a98ea
savchenkoadd <savchenkooad@gmail.com>
2024-01-30 07:17:38 +0200

    Add JWT service interface

---
+++ src/Flashcards.WebAPI/Flashcards.Core/ServiceContracts/IJwtService.cs
@@ -0,0 +1,10 @@
+﻿using Flashcards.Core.Domain.Identity;
+using Flashcards.Core.DTO.JWT;
+
+namespace Flashcards.Core.ServiceContracts
+{
+    public interface IJwtService
+    {
+        AuthenticationResponse CreateJwtToken(ApplicationUser applicationUser);
+    }
+}
